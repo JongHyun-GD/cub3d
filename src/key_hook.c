c35af2df1234d93b63e71ecf4fc0a12130618bf4
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2022-02-09 13:31:27 +0900

    Fix border line issue

--- src/key_hook.c
+++ src/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: jongpark <jongpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 20:31:33 by hyun              #+#    #+#             */
-/*   Updated: 2022/02/09 13:23:57 by jongpark         ###   ########.fr       */
+/*   Updated: 2022/02/09 13:30:10 by jongpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ int	move_forward(t_info *info)
 	move_amount.x = round(info->p_dir.x * 10) / 10;
 	move_amount.y = round(info->p_dir.y * 10) / 10;
 	dest = v2_plus(info->p_pos, move_amount);
-	if ((int)round(dest.x * 10) % 10 != 0 && \
-		(int)round(dest.y * 10) % 10 != 0 && \
-		info->map_info.map[(int)dest.y][(int)dest.x] == FLOOR)
+	if ((int)round(dest.x * 10) % 10 == 0)
+		dest.x += 0.1;
+	if ((int)round(dest.y * 10) % 10 == 0)
+		dest.y += 0.1;
+	if (info->map_info.map[(int)(dest.y)][(int)(dest.x)] == FLOOR)
 	{
 		info->p_pos = dest;
 		render(info);
@@ -39,14 +41,16 @@ int	move_backward(t_info *info)
 	move_amount.x = round(info->p_dir.x * 10) / 10;
 	move_amount.y = round(info->p_dir.y * 10) / 10;
 	dest = v2_minus(info->p_pos, move_amount);
-	if ((int)round(dest.x * 10) % 10 != 0 && \
-		(int)round(dest.y * 10) % 10 != 0 && \
-		info->map_info.map[(int)dest.y][(int)dest.x] == FLOOR)
+	if ((int)round(dest.x * 10) % 10 == 0)
+		dest.x += 0.1;
+	if ((int)round(dest.y * 10) % 10 == 0)
+		dest.y += 0.1;
+	if (info->map_info.map[(int)(dest.y)][(int)(dest.x)] == FLOOR)
 	{
 		info->p_pos = dest;
 		render(info);
 	}
-	printf("Key S pressed\n");
+	printf("Key S pressed. p_pos(%f, %f)\n", info->p_pos.x, info->p_pos.y);
 	return (0);
 }
 
@@ -58,14 +62,16 @@ int	move_right(t_info *info)
 	move_amount.x = round(info->p_right.x * 10) / 10;
 	move_amount.y = round(info->p_right.y * 10) / 10;
 	dest = v2_plus(info->p_pos, move_amount);
-	if ((int)round(dest.x * 10) % 10 != 0 && \
-		(int)round(dest.y * 10) % 10 != 0 && \
-		info->map_info.map[(int)dest.y][(int)dest.x] == FLOOR)
+	if ((int)round(dest.x * 10) % 10 == 0)
+		dest.x += 0.1;
+	if ((int)round(dest.y * 10) % 10 == 0)
+		dest.y += 0.1;
+	if (info->map_info.map[(int)(dest.y)][(int)(dest.x)] == FLOOR)
 	{
 		info->p_pos = dest;
 		render(info);
 	}
-	printf("Key D pressed\n");
+	printf("Key D pressed. p_pos(%f, %f)\n", info->p_pos.x, info->p_pos.y);
 	return (0);
 }
 
@@ -77,14 +83,16 @@ int	move_left(t_info *info)
 	move_amount.x = round(info->p_right.x * 10) / 10;
 	move_amount.y = round(info->p_right.y * 10) / 10;
 	dest = v2_minus(info->p_pos, move_amount);
-	if ((int)round(dest.x * 10) % 10 != 0 && \
-		(int)round(dest.y * 10) % 10 != 0 && \
-		info->map_info.map[(int)round(dest.y)][(int)round(dest.x)] == FLOOR)
+	if ((int)round(dest.x * 10) % 10 == 0)
+		dest.x += 0.1;
+	if ((int)round(dest.y * 10) % 10 == 0)
+		dest.y += 0.1;
+	if (info->map_info.map[(int)(dest.y)][(int)(dest.x)] == FLOOR)
 	{
 		info->p_pos = dest;
 		render(info);
 	}
-	printf("Key A pressed\n");
+	printf("Key A pressed. p_pos(%f, %f)\n", info->p_pos.x, info->p_pos.y);
 	return (0);
 }
 
